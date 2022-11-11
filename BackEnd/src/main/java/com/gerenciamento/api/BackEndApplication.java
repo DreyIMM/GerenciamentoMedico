1c22b76d1de1722c5b48662e3e0f307cc1802826
DreyIMM <andreydario26@gmail.com>
2022-11-11 11:33:46 -0300

    Variaveis para os servidores

--- BackEnd/src/main/java/com/gerenciamento/api/BackEndApplication.java
+++ BackEnd/src/main/java/com/gerenciamento/api/BackEndApplication.java
@@ -38,8 +38,8 @@ public class BackEndApplication implements CommandLineRunner{
 	        @Override
 	        public void addCorsMappings(CorsRegistry registry) {
 	            registry.addMapping("/**")
-	                .allowedOrigins("http://localhost:4200")
-	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+						.allowedOrigins("*")
+	                	.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	        }
 	    }
 	
--- FrontEnd/ConsultaMedica/src/environments/environment.ts
+++ FrontEnd/ConsultaMedica/src/environments/environment.ts
@@ -8,4 +8,4 @@ export const environment = {
 };
 
 
-export const API_PATH = "http://localhost:8080/"
+export const API_PATH = "https://www.consultauva.tk/"

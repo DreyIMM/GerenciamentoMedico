251c673c3a1cd9f6f96a8d0f28988947b40d98e0
Fábio Bezerra <bezerradeoliveirafabio@gmail.com>
2022-11-24 20:06:13 -0300

    dialogs padrão,edit bugado e botao de sair

---
+++ FrontEnd/ConsultaMedica/src/app/categoria/dialogCategoria/dialogCategoria.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Inject} from '@angular/core';
+import { MatDialogRef , MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { CategoriaComponent } from '../categoria.component';
+import { CategoriaModel } from '../categoria.models';
+import { CategoriaService } from '../categoria.service';
+
+@Component({
+  selector: 'app-dialogcategoria',
+  templateUrl: './dialogcategoria.component.html',
+  styleUrls: ['./dialogCategoria.component.css']
+})
+export class DialogcategoriaComponent implements OnInit {
+
+  constructor(private categoriaService:CategoriaService, private dialogRef: MatDialogRef<DialogcategoriaComponent>,@Inject(MAT_DIALOG_DATA)public data:CategoriaComponent) { }
+  categoria: CategoriaModel= new CategoriaModel;
+  categorias: Array<any> = new Array();
+ 
+  isEditar = this.data;
+  ngOnInit(): void {
+    console.log(this.data)
+  }
+
+
+  listarCategorias(){
+    this.categoriaService.listarCategorias().subscribe(categorias =>{
+      this.categorias = categorias
+    }, err =>{
+        console.log("erro ao listar as categorias", err);
+    })
+  }
+
+  cadastrarCategoria(){
+    this.categoriaService.CadastrarCategoria(this.categoria).subscribe(categoria =>{
+    this.categoria = new CategoriaModel();
+    this.listarCategorias();
+    this.aoSalvarFechar();  
+      
+    }, err =>{
+      console.log('Error ao cadastrar a categoria', err)
+    })
+  }
+
+  editarCategoria(categoria: CategoriaModel){
+    console.log("a")
+  }
+
+
+  aoSalvarFechar(){
+    let ref= document.getElementById('voltar');
+    ref?.click()
+  }
+}
---
+++ FrontEnd/ConsultaMedica/src/app/medico/dialogMedico/dialogMedico.component.ts
@@ -0,0 +1,47 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MedicoService } from '../medico.service';
+import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MedicoModel} from '../medico.models';
+import { MedicoComponent } from '../medico.component';
+
+
+@Component({
+  selector: 'app-dialogmedico',
+  templateUrl: './dialogMedico.component.html',
+  styleUrls: ['./dialogMedico.component.css']
+})
+export class DialogMedicoComponent implements OnInit {
+
+  constructor(private medicoService:MedicoService, private dialogRef: MatDialogRef<DialogMedicoComponent>,@Inject(MAT_DIALOG_DATA)public data:MedicoComponent) { }
+  medicos: Array<any> = new Array();
+  medico: MedicoModel = new MedicoModel();
+  isEditar = this.data;
+  ngOnInit(): void {
+    
+  }
+  
+  listarMedicos(){
+    this.medicoService.listarMedicos().subscribe(medicos =>{
+        this.medicos = medicos;
+    }, err=>{
+      console.log("Erro ao listar os medicos", err);
+    })
+  }
+
+  aoSalvarFechar(){
+    let ref= document.getElementById('voltar');
+    ref?.click()
+  }
+
+  CadastrarMedicos(){
+    this.medicoService.CadastrarMedicos(this.medico).subscribe(medico =>{
+    this.medico = new MedicoModel();
+    this.listarMedicos();
+    this.aoSalvarFechar();
+    }, err =>{
+      console.log('Error ao cadastrar um medico', err)
+    })
+    console.log(this.medicos)
+  }
+  
+}
